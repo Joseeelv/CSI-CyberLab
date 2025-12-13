749d2498780b3abc4225fd32c4d4b00d10c46907
Joseelv_ <jose.venegasan@gmail.com>
2025-12-13 13:25:09 +0100

    Minor fixes

--- backend/src/user-labs/user-lab.entity.ts
+++ backend/src/user-labs/user-lab.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { User } from '../users/user.entity';
 import { Lab } from '../labs/lab.entity';
 
