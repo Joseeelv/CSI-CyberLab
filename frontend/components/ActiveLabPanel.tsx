25efddcb7f163d041113227b3b3269b5ab00583b
Joseelv_ <jose.venegasan@gmail.com>
2025-12-16 00:50:55 +0100

    mejora la definición de la interfaz User y actualiza las dependencias de pg en pnpm-lock.yaml

--- frontend/components/ActiveLabPanel.tsx
+++ frontend/components/ActiveLabPanel.tsx
@@ -7,7 +7,7 @@ import { Lab, ActiveLab } from '@/types/lab';
 interface User {
   id: string;
   name?: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }
 
 interface ActiveLabPanelProps {
